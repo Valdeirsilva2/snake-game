6655325f21e13ff19e339edf9c03b9c01b7f3869
Valdeir Silva <valdeir.jobs@gmail.com>
2021-04-03 00:43:34 -0300

    regras dos movimentos da cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 16;
 let snake = [];
 
 snake[0] = {
@@ -22,7 +22,23 @@ function createSnake() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(e) {
+    if(e.keyCode == 37 && direction != "right") direction = "left";
+    if(e.keyCode == 38 && direction != "down") direction = "up";
+    if(e.keyCode == 39 && direction != "left") direction = "right";
+    if(e.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame() {
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+
     criarBG();
     createSnake();
 
