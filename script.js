7602422d32266e6d5aeca719585dbef2e5d97994
Valdeir Silva <valdeir.jobs@gmail.com>
2021-04-02 23:57:08 -0300

    criando o movimento da cobrinha

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "Lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -16,11 +18,33 @@ function criarBG() {
 function createSnake() {
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box)
-        
+        context.fillRect(snake[i].x, snake[i].y, box, box);        
     }
 }
 
+function startGame() {
+    criarBG();
+    createSnake();
 
-criarBG();
-createSnake();
+    let snakeX = snake[0].x; 
+    let snakeY =  snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let game = setInterval(startGame, 100);
+// startGame();
